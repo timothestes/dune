5b182037ebcac56c374143c0b1f2e0547d578d05
Tim E <tim.estes@br.iq>
2023-01-12 23:35:26 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.game.board.locations.arrakis import Arrakis
 from game.pieces.agents.agent import Agent
 from src.game.board.board import Board
+
 if __name__ == "__main__":
 
     # Create a new agent
@@ -19,4 +20,4 @@ if __name__ == "__main__":
     board.move_agent(blue_agent, arrakis)
 
     # end the turn, recalling all agents
-    board.recall_agents() 
+    board.recall_agents()
