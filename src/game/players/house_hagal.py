75a07e936ae842feb1067f1bb7cf3ff08dbfe726
Tim E <tim.estes@br.iq>
2023-01-16 12:32:52 -0800

    start working on influence

---
+++ src/game/players/house_hagal.py
@@ -0,0 +1,11 @@
+from src.game.players.player import Player
+from src.game.pieces.resources.manager import ResourceManager
+
+
+class HouseHagalPlayer(Player):
+    def __init__(self, color: str, player_id: int = None):
+        super().__init__(color, player_id)
+
+    def get_starting_resources(self):
+        starting_resources = {"water": 1, "spice": 0, "solari": 0}
+        return ResourceManager(self.color, **starting_resources)
---
+++ src/game/players/influence_manager.py
@@ -0,0 +1,12 @@
+class InfluenceManager:
+    def __init__(
+        self,
+        emperor: int = 0,
+        spacing_guild: int = 0,
+        bene_gesserit: int = 0,
+        fremen: int = 0,
+    ):
+        self.emperor = emperor
+        self.spacing_guild = spacing_guild
+        self.bene_gesserit = bene_gesserit
+        self.fremen = fremen
